ded2987cc84999ab6bb59dcedbc876b9ad21e645
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-08-24 19:46:29 -0300

    numero8.c

--- numero8.c
+++ numero8.c
@@ -8,11 +8,13 @@ main(){
 	printf("Digite o salario atual \n%f");
 	scanf("%f",&salarioAtual);
 	
-	printf("Digite o reajuste \n%f");
+	printf("Digite o reajuste: 2");
 	scanf("%f", &reajuste);
 	
-	printf("Calcule o novoSalario \n%.2x");
-	scanf("%.2x",&novoSalario);
+	printf("Calcule o novoSalario \n+400");
+	scanf("+400",&novoSalario);
+	
+	printf("O novo salario e:",novoSalario+salarioAtual,"%")
 	
 	return 0;
 }
